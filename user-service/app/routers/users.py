c7f7b1a83c867c3d5f5776f6dfc22f259f969233
Asif01933 <asifhayatanjon@gmail.com>
2025-07-26 10:57:21 +0600

    get user by id endpoint

--- user-service/app/routers/users.py
+++ user-service/app/routers/users.py
@@ -1,6 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from app.dependencies.auth import get_current_user
+from app.dependencies.roles import require_role
 from app.db.models.user import User
+from app.db.session import get_db
+from sqlalchemy.orm import Session
 
 router = APIRouter()
 
@@ -11,4 +14,21 @@ def read_me(current_user: User = Depends(get_current_user)):
         "username": current_user.username,
         "email": current_user.email,
         "role": current_user.role
+    }
+    
+@router.get("/users/{id}")
+def get_user_by_id(
+    id: int,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(require_role("admin"))
+):
+    user = db.query(User).filter(User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    return {
+        "id": user.id,
+        "username": user.username,
+        "email": user.email,
+        "role": user.role
     }
